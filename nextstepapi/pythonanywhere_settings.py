7f4e5e7aa798a0faa1d4ab34f0da4fb52ebd6e1b
mmagnusen <m-mags@hotmail.co.uk>
2018-06-16 20:18:33 +0100

    removed databse from pythonanywhere settings

--- nextstepapi/pythonanywhere_settings.py
+++ nextstepapi/pythonanywhere_settings.py
@@ -163,15 +163,15 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'nextstepreactdb',
-        'USER': 'marilynnextstepdbsuperuser',
-        'PASSWORD': 'sdfaercsertretsret234243@2',
-        'HOST': 'marilynmags-625.postgres.pythonanywhere-services.com',
-        'PORT': '10625',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'nextstepreactdb',
+#         'USER': 'marilynnextstepdbsuperuser',
+#         'PASSWORD': 'sdfaercsertretsret234243@2',
+#         'HOST': 'marilynmags-625.postgres.pythonanywhere-services.com',
+#         'PORT': '10625',
+#     }
+# }
 
 ALLOWED_HOSTS = ['www.thenextstep.io', 'marilynmags.pythonanywhere.com', 'thenextstep.io', '127.0.0.1', 'localhost']
